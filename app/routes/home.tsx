889ea948363bd27055c65ca0daea39a93eab2e93
Shruti Patil <shruticpatil2603@gmail.com>
2026-06-04 22:52:10 +0530

    wipe

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -67,8 +67,8 @@ export default function Home() {
       )}
 
       {!loadingResumes && resumes.length === 0 && (
-          <div className="flex flex-col items-center justify-center mt-10 gap-4">
-            <Link to="/upload" className="primary-Button w-fit text-xl font-semibold">
+          <div className ="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
               Upload Resume
             </Link>
           </div>
